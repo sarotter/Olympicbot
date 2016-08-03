058cb9d36e5353321ab759d8083dfd054bb7b30b
sarotter <sarotter@umich.edu>
2016-08-03 16:51:42 -0500

    Initial data scraping

---
+++ Olympicbot.R
@@ -0,0 +1,28 @@
+library(tm)
+library(RCurl)
+library(XML)
+library(rvest)
+library(RSelenium)
+library(stringr)
+library(stringi)
+library(SnowballC)
+library(devtools)
+library(lubridate)
+#Store current date
+date <- format(Sys.Date(),"%Y%m%d")
+
+#set url for day
+url = str_c("http://www.espn.com/olympics/summer/2016/schedule/_/date/", date, sep = "")
+
+#read in url and create the dataframe
+schedule <- read_html(url)
+schedule_df <- schedule %>% html_nodes("table") %>% .[[1]] %>% html_table(trim = FALSE)
+schedule_df[,c(1,5)] <- NULL
+
+#Set search critera for best games: first medal games then our rankings
+schedule %>% html_nodes("span") %>% html_attr("class") -> medal_games
+indices <- grep('olympics-medal gold', medal_games)
+ranking <- data.frame("sport" = c("Swimming", "Athletics", "Gymnastics", "Basketball", "Soccer", "Volleyball", "Rowing", "Tennis", "Table Tennis"), "ranking" = 1:9)
+
+
+
