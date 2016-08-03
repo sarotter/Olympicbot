2f12012c8aebfd9577a7e2c1ef784997eae26d7c
sarotter <sarotter@umich.edu>
2016-08-03 17:28:31 -0500

    Fixed indexing

--- Olympicbot.R
+++ Olympicbot.R
@@ -9,7 +9,7 @@ library(SnowballC)
 library(devtools)
 library(lubridate)
 #Store current date
-date <- format(Sys.Date(),"%Y%m%d")
+date <- format(as.Date("2016-08-17"),"%Y%m%d")
 
 #set url for day
 url = str_c("http://www.espn.com/olympics/summer/2016/schedule/_/date/", date, sep = "")
@@ -20,9 +20,9 @@ schedule_df <- schedule %>% html_nodes("table") %>% .[[1]] %>% html_table(trim =
 schedule_df[,c(1,5)] <- NULL
 
 #Set search critera for best games: first medal games then our rankings
-schedule %>% html_nodes("span") %>% html_attr("class") -> medal_games
-indices <- grep('olympics-medal gold', medal_games)
+schedule %>% html_nodes("table") %>% .[[1]]  %>% html_nodes("tr") -> medal_games
+indices <- grep('class=\"olympics-medal gold\"', medal_games)
+indices - 1 -> indices
 ranking <- data.frame("sport" = c("Swimming", "Athletics", "Gymnastics", "Basketball", "Soccer", "Volleyball", "Rowing", "Tennis", "Table Tennis"), "ranking" = 1:9)
 
-
-
+schedule_df[indices,] -> relevant_games
