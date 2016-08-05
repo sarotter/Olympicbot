6b8d03376aff8895313b7c0451076f47ec893685
sarotter <sarotter@umich.edu>
2016-08-05 17:13:44 +0200

    Prepared to prepare to tweet

--- Olympicbot.R
+++ Olympicbot.R
@@ -8,8 +8,11 @@ library(stringi)
 library(SnowballC)
 library(devtools)
 library(lubridate)
+library(reshape2)
+library(tidyr)
+library(dplyr)
 #Store current date
-date <- format(Sys.Date(),"%Y%m%d")
+date <- format(Sys.Date()+2,"%Y%m%d")
 
 #set url for day
 url = str_c("http://www.espn.com/olympics/summer/2016/schedule/_/date/", date, sep = "")
@@ -23,9 +26,9 @@ schedule_df[,c(1,5)] <- NULL
 schedule %>% html_nodes("table") %>% .[[1]]  %>% html_nodes("tr") -> medal_games
 indices <- grep('class=\"olympics-medal gold\"', medal_games)
 indices - 1 -> indices
-ranking <- data.frame("sport" = c("Swimming", "Athletics", "Gymnastics", "Basketball", "Soccer", "Volleyball", "Rowing", "Tennis", "Table Tennis"), "ranking" = 1:9)
+ranking <- data.frame("Sport" = c("Swimming", "Athletics", "Gymnastics", "Basketball", "Soccer", "Volleyball", "Rowing", "Tennis", "Table Tennis", "Weightlifting", "Shooting", "Cycling - Road","Shooting", "Diving", "Judo", "Archery", "Fencing"), "ranking" = 1:17)
 
-schedule_df[indices,] -> relevant_games
+(schedule_df[indices,] -> relevant_games)
 
 (published_table <- merge (relevant_games, ranking))
 (published_table <- published_table %>% group_by(Sport) %>% summarise(ranking = max(ranking)) %>% arrange(ranking))
@@ -33,9 +36,8 @@ schedule_df[indices,] -> relevant_games
 
 #relevant_games %>% filter(Sport == published_table[1,1])
 
-#class(as.character(published_table[1,1]))
-
-try_table <- sapply(1:3, function(x) {
-  sport <- rbind(relevant_games %>% filter(Sport == as.character(published_table[x,1])))
+tweet_table <- sapply(1:3, function(x) {
+  sport <- relevant_games %>% filter(Sport == as.character(published_table[x,1]))
   return(sport[1,])
 })
+tweet_table <- t(tweet_table) %>% as.data.table(byrow=TRUE)
