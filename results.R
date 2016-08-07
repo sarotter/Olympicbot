5adf608499ba17be6226a87630cd6da5c2559790
sarotter <sarotter@umich.edu>
2016-08-08 00:42:02 +0200

    Update results.R

--- results.R
+++ results.R
@@ -1,4 +1,3 @@
-read_html("https://www.google.co.za/search?q=olympics+medal+count&oq=olympics+medal+co&aqs=chrome.0.0j69i57j0l4.6290j0j4&sourceid=chrome&ie=UTF-8#mie=oly%2C%5B%22%2Fm%2F03tnk7%22%2C1%2C%22m%22%2C1%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C0%5D") -> medal_count
-medal_count_df <- medal_count %>% html_nodes("div") %>% html_table(trim = FALSE, fil = TRUE)
+#WORKING IN PROGRESS
 
 
